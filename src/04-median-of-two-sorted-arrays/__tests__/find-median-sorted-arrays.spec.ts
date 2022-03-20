929d5c010f32ca0bbbe3adc9e94051dfd557cdf1
Lexus Drumgold <unicornware@flexdevelopment.llc>
2022-03-19 20:17:24 -0400

    feat(algs): `findMedianSortedArrays`
    - https://leetcode.com/problems/median-of-two-sorted-arrays

---
+++ src/04-median-of-two-sorted-arrays/__tests__/find-median-sorted-arrays.spec.ts
@@ -0,0 +1,27 @@
+/**
+ * @file Unit Tests - findMedianSortedArrays
+ * @module leetcode-algorithms/04/findMedianSortedArrays/tests/unit
+ */
+
+import type { Testcase } from '@tests/interfaces'
+import testSubject from '../find-median-sorted-arrays'
+
+describe('unit:04/findMedianSortedArrays', () => {
+  interface Case extends Testcase<ReturnType<typeof testSubject>> {
+    nums1: number[]
+    nums2: number[]
+  }
+
+  const cases: Case[] = [
+    { expected: 4, nums1: [], nums2: [4] },
+    { expected: 7, nums1: [7], nums2: [] },
+    { expected: 2, nums1: [1, 3], nums2: [2] },
+    { expected: 2.5, nums1: [1, 2], nums2: [3, 4] }
+  ]
+
+  cases.forEach(({ expected, nums1, nums2 }) => {
+    it(`should return ${expected} given [[${nums1}],[${nums2}]]`, () => {
+      expect(testSubject(nums1, nums2)).to.equal(expected)
+    })
+  })
+})
---
+++ src/04-median-of-two-sorted-arrays/find-median-sorted-arrays.ts
@@ -0,0 +1,38 @@
+/**
+ * @file findMedianSortedArrays
+ * @module leetcode-algorithms/04/findMedianSortedArrays
+ */
+
+/**
+ * Given two sorted arrays, `nums1` and `nums2`, the function returns the median
+ * of the two sorted arrays.
+ *
+ * @see {@link https://leetcode.com/problems/median-of-two-sorted-arrays}
+ *
+ * @example findMedianSortedArrays([], [4]) => 4
+ * @example findMedianSortedArrays([7], []) => 7
+ * @example findMedianSortedArrays([1,3], [2]) => 2
+ * @example findMedianSortedArrays([1,2], [3,4]) => 2.5
+ *
+ * @param {number[]} nums1 - First sorted array
+ * @param {number[]} nums2 - Second sorted array
+ * @return {number} Median of `nums1` and `nums2`
+ */
+function findMedianSortedArrays(nums1: number[], nums2: number[]): number {
+  // If nums1 is empty and nums2 has one item, return first value in nums2
+  if (nums1.length === 0 && nums2.length === 1) return nums2[0]!
+
+  // If nums2 is empty and nums1 has one item, return first value in nums1
+  if (nums2.length === 0 && nums1.length === 1) return nums1[0]!
+
+  /** @const {number[]} nums - All numbers (sorted) */
+  const nums = [...nums1, ...nums2].sort((n1, n2) => n1 - n2)
+
+  /** @const {number} mid - Midpoint in index */
+  const mid = Math.floor(nums.length / 2)
+
+  // If nums.length is odd, return midpoint value. Otherwise, calculate median
+  return nums.length % 2 !== 0 ? nums[mid]! : (nums[mid - 1]! + nums[mid]!) / 2
+}
+
+export default findMedianSortedArrays
